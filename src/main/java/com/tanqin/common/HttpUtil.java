199f0d1185b36370e644bd1813127b15265d754b
tanqin <cn.tanqin@qq.com>
2023-02-16 12:29:32 +0800

    chore: 添加 Http 工具方法

---
+++ src/main/java/com/tanqin/common/HttpUtil.java
@@ -0,0 +1,20 @@
+package com.tanqin.common;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Http 工具方法
+ */
+public class HttpUtil {
+    public static HttpServletRequest getRequest() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    }
+
+    public static HttpServletResponse getResponse() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+    }
+}
